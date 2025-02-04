745f3794597924cd2316c381db8e712ec61ad2a6
hotsauceandqueso <briankshaw1215@proton.me>
2025-02-04 18:21:28 -0500

    Update input.js

--- input.js
+++ input.js
@@ -29,3 +29,17 @@ document.getElementById("searchButton").onclick = async function (event) {
 
     document.getElementById("iframeWindow").src = __uv$config.prefix + __uv$config.encodeUrl(url);
 };
+
+
+
+document.getElementById("switcher").onchange = async function (event) {
+    switch (event.target.value) {
+        case "epoxy":
+            await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+            break;
+        case "bare":
+            await connection.setTransport("/baremod/index.mjs", [bareUrl]);
+            break;
+    }
+};
+
